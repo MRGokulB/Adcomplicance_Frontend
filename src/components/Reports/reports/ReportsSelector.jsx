78ea82db2a16a201743b45bc5ad9e453332bf3cf
MRGokulB <rdxdarth80@gmail.com>
2025-09-29 12:05:37 +0530

    fixed a bug in reports page

--- src/components/Reports/reports/ReportsSelector.jsx
+++ src/components/Reports/reports/ReportsSelector.jsx
@@ -141,22 +141,6 @@ const ReportSelector = ({
         </div>
       )}
 
-      {/* Role-specific information banners */}
-      {userRole === USER_ROLES.PRODUCT_USER && (selectedReportType === 'internal-tasks' || selectedReportType === 'exchange-tasks') && (
-        <div className="mt-2 p-3 bg-blue-50 border border-blue-200 rounded-lg">
-          <div className="flex">
-            <svg className="w-5 h-5 text-blue-600 mr-2 mt-0.5" fill="currentColor" viewBox="0 0 20 20">
-              <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
-            </svg>
-            <div>
-              <p className="text-sm text-blue-800">
-                <strong>Note:</strong> This report shows only tasks that are assigned to you or created by you.
-              </p>
-            </div>
-          </div>
-        </div>
-      )}
-
       {/* No access message */}
       {availableReports.length === 0 && (
         <div className="mt-4 p-4 bg-yellow-50 border border-yellow-200 rounded-lg">
